2c93bbba09c5ec29e40994af40308a1d21ef1b9d
raffaez <rafaeleasouzas@gmail.com>
2023-02-14 12:41:40 -0300

    feat: implement simple-typewriter

--- web/src/components/Title.tsx
+++ web/src/components/Title.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { useTypewriter } from "react-simple-typewriter";
 
-function Title({ children }: { children: React.ReactNode }) {
-  return <div className="text-6xl font-poppins font-bold">{children}</div>;
+type Props = {
+  title: string;
+};
+
+function Title({ title }: Props) {
+  const [text, helper] = useTypewriter({
+    words: [`${title.toLowerCase()}`],
+    loop: 1,
+    delaySpeed: 1000,
+  });
+
+  return (
+    <div className="text-7xl md:text-8xl ml-5 font-bold text-slate-700 dark:text-gray-50/80">
+      {"<"}
+      <span className="font-poppins">{text}</span>
+      {">"}
+    </div>
+  );
 }
 
 export default Title;
