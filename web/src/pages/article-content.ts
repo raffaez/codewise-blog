bd6fbb0d02afad70f71a3f879e5897d7e6b4fc27
raffaez <rafaeleasouzas@gmail.com>
2023-02-15 01:33:50 -0300

    chore: add more articles, change texts

--- web/src/pages/article-content.ts
+++ web/src/pages/article-content.ts
@@ -7,11 +7,11 @@ const articles = [
             `React is a popular JavaScript library for building user interfaces. Developed by Facebook, React makes it easy to create dynamic and interactive web applications with reusable UI components. Whether you're building a simple website or a complex single-page application, React provides a powerful set of tools to help you create fast, efficient, and scalable interfaces.`,
             `In this tutorial, we'll cover the basics of React, including how to set up your development environment, create and use components, manage state and props, and handle user events. By the end of this tutorial, you'll have a solid foundation in React and be ready to start building your own applications."`,
         ],
-        image: 'https://images.unsplash.com/photo-1633356122102-3fe601e05bd2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
-        category: ['front-end'],
+        image: 'https://global-uploads.webflow.com/618fa90c201104b94458e1fb/63986927bbfd604be1df34d7_10-Modern-react-Tools-that-companies-use-in-production_Main-image.jpg',
+        category: 'front-end',
     }, {
         name: 'mongodb',
-        title: 'Learn MongoDB',
+        title: 'Intro to MongoDB: Getting Started',
         content: [
             `Welcome to our MongoDB tutorial! Learn how to use this NoSQL database to build powerful and scalable applications. Let's dive into the basics!`,
             `MongoDB is a popular document-oriented NoSQL database that provides a flexible and scalable solution for storing and querying large volumes of data. Unlike traditional relational databases, MongoDB uses a JSON-like document model that allows for more natural and dynamic data representation, making it an ideal choice for many modern applications.`,
@@ -19,35 +19,85 @@ const articles = [
             `So, let's get started and learn how to use MongoDB to build fast and scalable applications!`,
         ],
         image: 'https://community-cdn-digitalocean-com.global.ssl.fastly.net/v3ksSgN3Wr5YK6z8RrTxCiqf',
-        category: ['back-end'],
+        category: 'back-end',
     }, {
         name: 'learn-node',
-        title: 'How to Build a Node Server',
+        title: `Node.js Essentials: Starter's Guide`,
         content: [
             `Welcome to our Node tutorial! Learn how to build scalable and high-performance web applications with Node.js, a popular server-side JavaScript runtime.`, `Node.js is built on top of the V8 JavaScript engine from Google, and provides a wide range of libraries and modules that can be used to build a variety of web and mobile applications.`,
             `In this tutorial, we will guide you through the basics of Node.js, including installation, setup, and building your first Node.js application. We will also cover key concepts like modules, streams, and event-driven programming, which are fundamental to building efficient and performant Node.js applications.`,
             `Whether you're new to Node.js or looking to expand your existing knowledge, this tutorial will provide you with the foundation you need to start building your own Node.js applications. So let's get started!`,
         ],
-        image: 'https://images.unsplash.com/photo-1627398242454-45a1465c2479?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
-        category: ['back-end'],
+        image: 'https://bs-uploads.toptal.io/blackfish-uploads/components/seo/content/og_image_file/og_image/1127977/secure-rest-api-in-nodejs-18f43b3033c239da5d2525cfd9fdc98f.png',
+        category: 'back-end',
     }, {
         name: 'learn-graphql',
-        title: 'Learn GraphQL',
+        title: 'GraphQL Basics: Quick Start Guide',
         content: [
             `Learn how to build more efficient and flexible APIs with our GraphQL tutorial. Discover the benefits of this modern query language for your projects.`,
             `Welcome to our GraphQL tutorial! GraphQL is a modern query language for APIs that provides a more efficient and flexible way of communicating between the client and the server. With GraphQL, clients can define the shape and structure of the data they need, and the server can respond with only the requested data. This allows for more precise data fetching and reduces the amount of network traffic required, resulting in faster and more efficient applications.`, `In this tutorial, you'll learn the basics of GraphQL and how to use it to build powerful and scalable APIs. We'll cover everything from setting up a GraphQL server and defining a schema to executing queries and mutations and integrating with other technologies. Whether you're new to GraphQL or an experienced developer looking to expand your skills, this tutorial has something for everyone.`, `So, let's dive into the world of GraphQL and see how it can transform the way you build web applications!`,
         ],
         image: 'https://cdn.codersociety.com/uploads/graphql-reasons.png',
-        category: ['back-end'],
+        category: 'back-end',
     }, {
         name: 'learn-angular',
-        title: 'Learn Angular',
+        title: 'Angular Basics: Quick Start Guide',
         content: [
-            `Get started with Angular, a popular framework for building dynamic and robust web applications. This tutorial covers the basics of Angular, including components, services, and data binding.`,
+            `Learn Angular, a powerful framework for building web apps. This tutorial covers basics like components, services, and data binding.`,
             `Welcome to our Angular tutorial! Angular is a popular web application framework that allows developers to create dynamic and robust applications with ease. Developed and maintained by Google, Angular provides a comprehensive set of tools and features that streamline the development process and enable developers to create modern, scalable, and maintainable applications.`, `In this tutorial, we will cover the basics of Angular, including setting up a development environment, creating components, handling data, and working with services. By the end of this tutorial, you will have a solid foundation in Angular development and be able to create your own Angular applications.`, `Whether you are a seasoned web developer or just starting out, this tutorial will provide you with the knowledge and skills you need to get started with Angular. So let's dive in and get started with building awesome Angular applications!`,
         ],
         image: 'https://preprod.grazitti.com/assets/2018/06/8-Proven-Reasons-You-Need-Angular-02.png',
-        category: ['front-end'],
+        category: 'front-end',
+    },
+    {
+        name: 'learn-react',
+        title: 'The Fastest Way to Learn React',
+        content: [
+            `Learn React, the popular JavaScript library for building user interfaces. Create fast, scalable web apps with reusable components and efficient state management.`,
+            `React is a popular JavaScript library for building user interfaces. Developed by Facebook, React makes it easy to create dynamic and interactive web applications with reusable UI components. Whether you're building a simple website or a complex single-page application, React provides a powerful set of tools to help you create fast, efficient, and scalable interfaces.`,
+            `In this tutorial, we'll cover the basics of React, including how to set up your development environment, create and use components, manage state and props, and handle user events. By the end of this tutorial, you'll have a solid foundation in React and be ready to start building your own applications."`,
+        ],
+        image: 'https://global-uploads.webflow.com/618fa90c201104b94458e1fb/63986927bbfd604be1df34d7_10-Modern-react-Tools-that-companies-use-in-production_Main-image.jpg',
+        category: 'front-end',
+    }, {
+        name: 'mongodb',
+        title: 'Intro to MongoDB: Getting Started',
+        content: [
+            `Welcome to our MongoDB tutorial! Learn how to use this NoSQL database to build powerful and scalable applications. Let's dive into the basics!`,
+            `MongoDB is a popular document-oriented NoSQL database that provides a flexible and scalable solution for storing and querying large volumes of data. Unlike traditional relational databases, MongoDB uses a JSON-like document model that allows for more natural and dynamic data representation, making it an ideal choice for many modern applications.`,
+            `In this tutorial, we will guide you through the process of working with MongoDB, from installation and configuration to data modeling, querying, and performance optimization. Whether you're new to MongoDB or have some experience with NoSQL databases, this tutorial will provide a comprehensive overview of MongoDB's features and capabilities.`,
+            `So, let's get started and learn how to use MongoDB to build fast and scalable applications!`,
+        ],
+        image: 'https://community-cdn-digitalocean-com.global.ssl.fastly.net/v3ksSgN3Wr5YK6z8RrTxCiqf',
+        category: 'back-end',
+    }, {
+        name: 'learn-node',
+        title: `Node.js Essentials: Starter's Guide`,
+        content: [
+            `Welcome to our Node tutorial! Learn how to build scalable and high-performance web applications with Node.js, a popular server-side JavaScript runtime.`, `Node.js is built on top of the V8 JavaScript engine from Google, and provides a wide range of libraries and modules that can be used to build a variety of web and mobile applications.`,
+            `In this tutorial, we will guide you through the basics of Node.js, including installation, setup, and building your first Node.js application. We will also cover key concepts like modules, streams, and event-driven programming, which are fundamental to building efficient and performant Node.js applications.`,
+            `Whether you're new to Node.js or looking to expand your existing knowledge, this tutorial will provide you with the foundation you need to start building your own Node.js applications. So let's get started!`,
+        ],
+        image: 'https://bs-uploads.toptal.io/blackfish-uploads/components/seo/content/og_image_file/og_image/1127977/secure-rest-api-in-nodejs-18f43b3033c239da5d2525cfd9fdc98f.png',
+        category: 'back-end',
+    }, {
+        name: 'learn-graphql',
+        title: 'GraphQL Basics: Quick Start Guide',
+        content: [
+            `Learn how to build more efficient and flexible APIs with our GraphQL tutorial. Discover the benefits of this modern query language for your projects.`,
+            `Welcome to our GraphQL tutorial! GraphQL is a modern query language for APIs that provides a more efficient and flexible way of communicating between the client and the server. With GraphQL, clients can define the shape and structure of the data they need, and the server can respond with only the requested data. This allows for more precise data fetching and reduces the amount of network traffic required, resulting in faster and more efficient applications.`, `In this tutorial, you'll learn the basics of GraphQL and how to use it to build powerful and scalable APIs. We'll cover everything from setting up a GraphQL server and defining a schema to executing queries and mutations and integrating with other technologies. Whether you're new to GraphQL or an experienced developer looking to expand your skills, this tutorial has something for everyone.`, `So, let's dive into the world of GraphQL and see how it can transform the way you build web applications!`,
+        ],
+        image: 'https://cdn.codersociety.com/uploads/graphql-reasons.png',
+        category: 'back-end',
+    }, {
+        name: 'learn-angular',
+        title: 'Angular Basics: Quick Start Guide',
+        content: [
+            `Learn Angular, a powerful framework for building web apps. This tutorial covers basics like components, services, and data binding.`,
+            `Welcome to our Angular tutorial! Angular is a popular web application framework that allows developers to create dynamic and robust applications with ease. Developed and maintained by Google, Angular provides a comprehensive set of tools and features that streamline the development process and enable developers to create modern, scalable, and maintainable applications.`, `In this tutorial, we will cover the basics of Angular, including setting up a development environment, creating components, handling data, and working with services. By the end of this tutorial, you will have a solid foundation in Angular development and be able to create your own Angular applications.`, `Whether you are a seasoned web developer or just starting out, this tutorial will provide you with the knowledge and skills you need to get started with Angular. So let's dive in and get started with building awesome Angular applications!`,
+        ],
+        image: 'https://preprod.grazitti.com/assets/2018/06/8-Proven-Reasons-You-Need-Angular-02.png',
+        category: 'front-end',
     }
 ];
 
